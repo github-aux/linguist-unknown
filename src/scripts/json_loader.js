e7c958dc85957b5f4109e9670eb304be36601ace
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2016-10-19 03:47:34 -0700

    adding comments to the test language

--- src/scripts/json_loader.js
+++ src/scripts/json_loader.js
@@ -1,14 +1,19 @@
 chrome.storage.sync.get("shouldWork", function(items) {
   if (items.shouldWork) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json", true);
-    var resp;
-    xhr.onload = function () {
-      resp = JSON.parse(xhr.responseText);
-      console.log(resp);
-    }
-    xhr.setRequestHeader("Content-Type", "*/*");
-    xhr.send();
+    parseJson();
   }
 });
 
+function parseJson() {
+   var xhr = new XMLHttpRequest();
+   var url = "https://raw.githubusercontent.com/luizperes/linguist-unknown/master/lib/languages.json";
+   xhr.open("GET", url, true);
+   var resp;
+   xhr.onload = function () {
+     resp = JSON.parse(xhr.responseText);
+     console.log(resp);
+   }
+
+   xhr.setRequestHeader("Content-Type", "*/*");
+   xhr.send();
+}
