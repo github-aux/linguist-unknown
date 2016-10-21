e4cf4f075e26ecad2552c5f10a3084299dd200e7
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2016-10-20 21:59:09 -0700

    Changing project structure

--- src/scripts/json_loader.js
+++ src/scripts/json_loader.js
@@ -1,5 +1,5 @@
 var url_ext = "https://raw.githubusercontent.com/luizperes/linguist-unknown/master/lib/extensions.json";
-var url_lang = "https://raw.githubusercontent.com/luizperes/linguist-unknown/master/lib/languages.json";
+var url_lang_base = "https://raw.githubusercontent.com/luizperes/linguist-unknown/master/lib/languages/";
 var current_url = "";
 
 function JsonHelper() {
@@ -30,45 +30,44 @@ function Utilities() {
             });
         });
     };
-   
+
     this.getLanguageDetails = function(url, ext, callback) {
         var jsonHelper = new JsonHelper();
-        jsonHelper.load(url, function(objs){
-            objs.every(function(objLang){
-                if (objLang.language.valueOf() == ext.language.valueOf()) {
-                    // add extensions to the object just in case
-                    objLang.extensions = ext.extensions;
-                    callback(objLang);
-                    // that will break the "loop"
-                    return false;
-                }
-                
-                return true;
-            });
+        var newUrl = url + ext.language.toLowerCase() + ".json";
+        jsonHelper.load(newUrl, function(objLang){
+            // add extensions to the object just in case
+            objLang.extensions = ext.extensions;
+            callback(objLang);
         });
+    };
+
+    this.refresh = function() {
+        var new_url = window.location.href;
+        if (current_url != new_url) {
+            current_url = new_url;
+            window.setTimeout(function() {
+                var jsonHelper = new JsonHelper();
+                jsonHelper.load(url_ext, function(objs){
+                    var util = new Utilities();
+                    util.tryMatchUrlExtension(current_url, objs, function(obj){
+                        util.getLanguageDetails(url_lang_base, obj, function(langObj){
+                            new Highlighter(langObj).draw();
+                        });
+                    });
+                });
+            }, 100);
+        }
     };
 }
 
-chrome.storage.sync.get("shouldWork", function(items) {
-    if (items.shouldWork) {
-        // listening the webbrowser url change...
-        document.body.addEventListener('DOMSubtreeModified', function () {
-           var new_url = window.location.href;
-           if (current_url != new_url) {
-               current_url = new_url;
-               window.setTimeout(function() {
-                   var jsonHelper = new JsonHelper();
-                   jsonHelper.load(url_ext, function(objs){
-                       var util = new Utilities();
-                       util.tryMatchUrlExtension(current_url, objs, function(obj){
-                           util.getLanguageDetails(url_lang, obj, function(langObj){
-                               new Highlighter(langObj).draw(); 
-                           });
-                       }); 
-                   });
-               }, 100);
-           }
-        }, false);
-    }
+document.addEventListener('DOMContentLoaded', function(){
+    chrome.storage.sync.get("shouldWork", function(items) {
+        if (items.shouldWork) {
+            new Utilities().refresh();
+            // listening the webbrowser url change...
+            document.body.addEventListener('DOMSubtreeModified', function () {
+                new Utilities().refresh();
+            }, false);
+        }
+    });
 });
-
