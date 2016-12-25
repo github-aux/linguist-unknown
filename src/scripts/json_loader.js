7a3e61cceec487f331d88758defde29afda64e56
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2016-12-25 13:36:55 -0800

    Starting to think in how to include the yml maker

--- src/scripts/json_loader.js
+++ src/scripts/json_loader.js
@@ -2,13 +2,13 @@ var url_ext = "https://raw.githubusercontent.com/luizperes/linguist-unknown/mast
 var url_lang_base = "https://raw.githubusercontent.com/luizperes/linguist-unknown/master/lib/languages/";
 var current_url = "";
 
-function JsonHelper() {
+function DownloadHelper() {
     this.load = function(url, callback) {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url, true);
         xhr.onload = function () {
             try {
-                var resp = JSON.parse(xhr.responseText);
+                var resp = jsyaml.load(xhr.responseText);
                 callback(resp);
             } catch(e) {
                 console.error(e);
@@ -48,22 +48,30 @@ function Utilities() {
         });
     };
 
+    // this.refresh = function() {
+    //     var new_url = window.location.href;
+    //     if (current_url != new_url) {
+    //         current_url = new_url;
+    //         window.setTimeout(function() {
+    //             var jsonHelper = new JsonHelper();
+    //             jsonHelper.load(url_ext, function(objs){
+    //                 var util = new Utilities();
+    //                 util.tryMatchUrlExtension(current_url, objs, function(obj){
+    //                     util.getLanguageDetails(url_lang_base, obj, function(langObj){
+    //                         new Highlighter(langObj).draw();
+    //                     });
+    //                 });
+    //             });
+    //         }, 100);
+    //     }
+    // };
+
     this.refresh = function() {
-        var new_url = window.location.href;
-        if (current_url != new_url) {
-            current_url = new_url;
-            window.setTimeout(function() {
-                var jsonHelper = new JsonHelper();
-                jsonHelper.load(url_ext, function(objs){
-                    var util = new Utilities();
-                    util.tryMatchUrlExtension(current_url, objs, function(obj){
-                        util.getLanguageDetails(url_lang_base, obj, function(langObj){
-                            new Highlighter(langObj).draw();
-                        });
-                    });
-                });
-            }, 100);
-        }
+        var url = "https://raw.githubusercontent.com/brain-labs/brain/master/.travis.yml";
+        var downloadHelper = new DownloadHelper();
+        downloadHelper.load(url, function(objs){
+            console.log(objs);
+        });
     };
 }
 
