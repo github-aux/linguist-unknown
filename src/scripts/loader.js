17fa035ba7790958f8d840a3a9e1a13b938f4f0e
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-05-31 15:33:46 -0700

    Started to separate the files in order to make it better

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -75,14 +75,3 @@ function Utilities() {
     };
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-    chrome.storage.sync.get("shouldWork", function(items) {
-        if (items.shouldWork) {
-            new Utilities().refresh();
-            // listening the webbrowser url change...
-            document.body.addEventListener('DOMSubtreeModified', function () {
-                new Utilities().refresh();
-            }, false);
-        }
-    });
-});
