8591182a035fc90996edc1b3ed85659138f52c9c
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-06-04 01:20:29 -0700

    loader apparently done

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -35,11 +35,14 @@ var LinguistLoader = (function() {
   var Utilities = (function() {
     var Utilities = function() { };
 
-    Utilities.prototype.tryMatchUrlExtension = function(url, exts, successCallback) {
-        exts.every(function(lang){
-            var still_looking_for = true;
-            lang.extensions.every(function(obj) {
-                if (url.endsWith(obj.extension)) {
+    Utilities.prototype.tryMatchUrlExtension = function(url, objs, successCallback) {
+        var keys = Object.keys(objs);
+        var i = 0;
+        var still_looking_for = true;
+        while ((langKey = keys[i++]) && still_looking_for) {
+            var lang = objs[langKey];
+            lang.extensions.every(function(extension) {
+                if (url.endsWith(extension)) {
                    successCallback(lang);
                    still_looking_for = false;
                    return false;
@@ -47,19 +50,7 @@ var LinguistLoader = (function() {
 
                 return true;
             });
-
-            return still_looking_for;
-        });
-    };
-
-    Utilities.prototype.getLanguageDetails = function(url, ext, callback) {
-        var jsonHelper = new JsonHelper();
-        var newUrl = url + ext.language.toLowerCase() + ".json";
-        jsonHelper.load(newUrl, function(objLang){
-            // add extensions to the object just in case
-            objLang.extensions = ext.extensions;
-            callback(objLang);
-        });
+        }
     };
 
     Utilities.prototype.isGithub = function(l) {
@@ -84,31 +75,16 @@ var LinguistLoader = (function() {
           };
         }
 
+        var that = this;
         window.setTimeout(function() {
             var downloadHelper = new DownloadHelper();
             downloadHelper.load(linguistObj.path, function(objs){
-                console.log(objs);
+                that.tryMatchUrlExtension(current_url, objs, function(langObj){
+                    new Highlighter(langObj).draw();
+                });
             });
         }, 100);
     };
-
-    // this.refresh = function() {
-    //     var new_url = window.location.href;
-    //     if (current_url != new_url) {
-    //         current_url = new_url;
-    //         window.setTimeout(function() {
-    //             var jsonHelper = new JsonHelper();
-    //             jsonHelper.load(url_ext, function(objs){
-    //                 var util = new Utilities();
-    //                 util.tryMatchUrlExtension(current_url, objs, function(obj){
-    //                     util.getLanguageDetails(url_lang_base, obj, function(langObj){
-    //                         new Highlighter(langObj).draw();
-    //                     });
-    //                 });
-    //             });
-    //         }, 100);
-    //     }
-    // };
 
     return Utilities;
   }());
