81955d89d7616ea8704e9d76fed1d7bc5b5390bf
Thales Caricati <thales.caricati@gmail.com>
2017-06-07 23:38:33 -0300

    Apply airbnb guide lines.

--- src/scripts/ling-loader.js
+++ src/scripts/ling-loader.js
@@ -1,8 +1,10 @@
 var LinguistLoader = (function() {
-  const GITHUB_HOST = "github.com";
-  const GITHUB_URL_FILE = "blob/";
-  const GITHUB_RAW = "https://raw.githubusercontent.com";
-  const FILENAME = ".linguist.yml";
+  'use strict';
+
+  var GITHUB_HOST = "github.com";
+  var GITHUB_URL_FILE = "blob/";
+  var GITHUB_RAW = "https://raw.githubusercontent.com";
+  var FILENAME = ".linguist.yml";
 
   var linguistObj = null;
   var current_url = "";
@@ -10,23 +12,23 @@ var LinguistLoader = (function() {
   var DownloadHelper = (function() {
     var DownloadHelper = function() { };
     DownloadHelper.prototype.load = function(url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.onload = function () {
-            try {
-                if (xhr.status === 404) {
-                    return; // ignore if file is not found
-                }
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url, true);
+      xhr.onload = function () {
+        try {
+          if (xhr.status === 404) {
+            return; // ignore if file is not found
+          }
 
-                var resp = jsyaml.load(xhr.responseText);
-                callback(resp);
-            } catch(e) {
-                console.error(e);
-            }
+          var resp = jsyaml.load(xhr.responseText);
+          callback(resp);
+        } catch(e) {
+          console.error(e);
         }
+      };
 
-        xhr.setRequestHeader("Content-Type", "*/*");
-        xhr.send();
+      xhr.setRequestHeader("Content-Type", "*/*");
+      xhr.send();
     };
 
     return DownloadHelper;
@@ -36,54 +38,59 @@ var LinguistLoader = (function() {
     var Utilities = function() { };
 
     Utilities.prototype.tryMatchUrlExtension = function(url, objs, successCallback) {
-        var keys = Object.keys(objs);
-        var i = 0;
-        var still_looking_for = true;
-        while ((langKey = keys[i++]) && still_looking_for) {
-            var lang = objs[langKey];
-            lang.extensions.every(function(extension) {
-                if (url.endsWith(extension)) {
-                   successCallback(lang);
-                   still_looking_for = false;
-                   return false;
-                }
+      var keys = Object.keys(objs);
+      var i = 0;
+      var still_looking_for = true;
+      var lang;
 
-                return true;
-            });
+      // Don't make a function within a loop
+      var extFunc = function(extension) {
+        if (url.endsWith(extension)) {
+          successCallback(lang);
+          still_looking_for = false;
+          return false;
         }
+
+        return true;
+      };
+
+      while ((langKey = keys[i++]) && still_looking_for) {
+        lang = objs[langKey];
+        lang.extensions.every(extFunc);
+      }
     };
 
     Utilities.prototype.isGithub = function(l) {
-        return l.hostname === GITHUB_HOST && l.href.includes(GITHUB_URL_FILE);
+      return l.hostname === GITHUB_HOST && l.href.includes(GITHUB_URL_FILE);
     };
 
     Utilities.prototype.getPossibleFilepath = function(l) {
-        var uri = l.pathname.split(GITHUB_URL_FILE);
-        return GITHUB_RAW + uri[0] + uri[1].split('/')[0] + '/' + FILENAME;
+      var uri = l.pathname.split(GITHUB_URL_FILE);
+      return GITHUB_RAW + uri[0] + uri[1].split('/')[0] + '/' + FILENAME;
     };
 
     Utilities.prototype.refresh = function() {
-        var new_url = window.location.href;
-        if (new_url === current_url || !this.isGithub(window.location)) {
-            return;
-        }
+      var new_url = window.location.href;
 
-        current_url = new_url;
-        if (linguistObj === null) {
-          linguistObj = {
-              path: this.getPossibleFilepath(window.location)
-          };
-        }
+      if (new_url === current_url || !this.isGithub(window.location)) {
+        return;
+      }
 
-        var that = this;
-        window.setTimeout(function() {
-            var downloadHelper = new DownloadHelper();
-            downloadHelper.load(linguistObj.path, function(objs){
-                that.tryMatchUrlExtension(current_url, objs, function(langObj){
-                    new LinguistHighlighter.Highlighter(langObj).draw();
-                });
-            });
-        }, 100);
+      current_url = new_url;
+      if (linguistObj === null) {
+        linguistObj = {
+          path: this.getPossibleFilepath(window.location)
+        };
+      }
+
+      window.setTimeout(function() {
+        var downloadHelper = new DownloadHelper();
+        downloadHelper.load(linguistObj.path, function(objs){
+          this.tryMatchUrlExtension(current_url, objs, function(langObj){
+            new LinguistHighlighter.Highlighter(langObj).draw();
+          });
+        }.bind(this));
+      }.bind(this), 100);
     };
 
     return Utilities;
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,32 +1,31 @@
-function save_options() {
-    var active = document.getElementById('main-checkbox').checked;
-    chrome.storage.sync.set({"shouldWork": active}, function() {
-        //console.log('Settings saved');
-    });
+(function(doc, browser){
+  'use strict';
+
+  function save_options() {
+    var active = doc.getElementById('main-checkbox').checked;
+    browser.storage.sync.set({ 'shouldWork': active }, function() {});
 
     // refresh page
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var code = 'window.location.reload();';
-        chrome.tabs.executeScript(tabs[0].id, {code: code});
+    browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      var code = 'window.location.reload();';
+      browser.tabs.executeScript(tabs[0].id, { code: code });
     });
-}
+  }
 
-function restore_options() {
-    chrome.storage.sync.get("shouldWork", function(items) {
-        var checkbox = document.getElementById('main-checkbox');
-        if (checkbox) {
-            checkbox.checked = items.shouldWork;
-            //console.log(items);
-            //console.log(items.shouldWork);
-        }
+  function restore_options() {
+    browser.storage.sync.get('shouldWork', function(items) {
+      var checkbox = doc.getElementById('main-checkbox');
+      if (checkbox) {
+        checkbox.checked = items.shouldWork;
+      }
     });
-}
+  }
 
-document.addEventListener('DOMContentLoaded', function() {
+  doc.addEventListener('DOMContentLoaded', function() {
     restore_options();
-    document.getElementById('main-checkbox').addEventListener('change',
-        save_options,
-        false
+    doc.getElementById('main-checkbox').addEventListener('change',
+      save_options,
+      false
     );
-});
-
+  });
+}(document, chrome));
